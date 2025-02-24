264cad0c373ab902dabaa71b7ae6217e45876b6d
Djebbi <djebbifarouk@gmail.com>
2025-02-24 16:06:30 +0100

    Creation du projet first commit

---
+++ lesgriots/Models/AdminAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesgriots.Models;
+
+public partial class AdminAccount
+{
+    public int Id { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string? TwoFactorSecret { get; set; }
+
+    public bool? IsDeleted { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ lesgriots/Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesgriots.Models;
+
+public partial class Transaction
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public Guid SubscriptionId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string? Currency { get; set; }
+
+    public byte Status { get; set; }
+
+    public DateTime? TransactionDate { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
